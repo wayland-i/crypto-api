98c755cf938ad4533c9c78ccec89219717669bbf
Ian Iversen <ianiversen1@gmail.com>
2023-02-11 11:50:12 -0500

    fixed: compiler error in application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,15 @@ module CryptoApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # or specific origins
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -37,13 +46,5 @@ module CryptoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
-end
-
-
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*' # or specific origins
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
