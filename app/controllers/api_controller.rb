e29df61cb272c4baab93cc9747b85b6a8496fff2
Ian Iversen <ianiversen1@gmail.com>
2023-02-11 13:55:07 -0500

    JSON: I think I fixed the bug

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -16,7 +16,7 @@ class ApiController < ApplicationController
         api_key = ENV['API_KEY']
         response = HTTParty.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=bitcoin', headers: {'X-CMC_PRO_API_KEY' => api_key})
         
-        render json: {data: response.body}
+        render json: response.body
 
         #this was my best attempt before I found the 'rack-cors' gem
         #render json: {data: response.body}, headers: {'Access-Control-Allow-Origin' => 'http://localhost:4000'}
