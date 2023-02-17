8955f50b16b2be954514c5ff846e5bcfea52f7db
Ian Iversen <ianiversen1@gmail.com>
2023-02-17 10:58:13 -0500

    feat: backend for snake leaderboard complete

---
+++ app/controllers/leaderboards_controller.rb
@@ -0,0 +1,50 @@
+class LeaderboardsController < ApplicationController
+  before_action :set_leaderboard, only: %i[ show update destroy ]
+
+  # GET /leaderboards
+  def index
+    @leaderboards = Leaderboard.order(score: :desc)
+    render json: @leaderboards
+  end
+
+  # GET /leaderboards/1
+  def show
+    render json: @leaderboard
+  end
+
+  # POST /leaderboards
+  def create
+    @leaderboard = Leaderboard.new(leaderboard_params)
+
+    if @leaderboard.save
+      render json: @leaderboard, status: :created, location: @leaderboard
+    else
+      render json: @leaderboard.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /leaderboards/1
+  def update
+    if @leaderboard.update(leaderboard_params)
+      render json: @leaderboard
+    else
+      render json: @leaderboard.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /leaderboards/1
+  def destroy
+    @leaderboard.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_leaderboard
+      @leaderboard = Leaderboard.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def leaderboard_params
+      params.require(:leaderboard).permit(:player, :score)
+    end
+end
